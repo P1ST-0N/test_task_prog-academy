2586c74698d1650885b310ab852fd5c923c2b9b3
P1ST-0N <luckylionia@gmail.com>
2022-09-14 15:10:26 +0300

    add html

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function sumBitcoins( bitcoinPrice, sumDollars) {
   let totalSum = 0;
   return totalSum = sumDollars / bitcoinPrice;
 }
-console.log(sumBitcoins(21000, 1000));
+
+let i1 = document.getElementById("i1");
+let i2 = document.getElementById("i2");
+let button = document.getElementById("button");
+
+button.addEventListener("click", calculateBitcoin);
+
+function calculateBitcoin() {
+  alert(sumBitcoins(i1.value, i2.value));
+}
